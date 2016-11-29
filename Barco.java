886892a61f2478c64bbce9cf9057400c5666544c
kfelsner <kristofersolerfelsner@protonmail.com>
2016-11-29 09:10:56 +0100

    Creación de la clase Barco

---
+++ Barco.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hundirlaflota;
+
+/**
+ *
+ * @author kfelsner
+ */
+public class Barco {
+    
+    //--------Declaraciones--------//
+    private int medida;
+    private enum barcos {portaaviones, buque, lancha};
+    private boolean hundido = false;
+    private int [] [] posicionesOcupadas;
+    private boolean [] [] posicionesFerides;
+ }
