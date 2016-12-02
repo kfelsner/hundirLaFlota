2edccbb6923d6f8f50362545a012918c3ca90ba0
kfelsner <kristofersolerfelsner@protonmail.com>
2016-12-02 10:27:58 +0100

    Separacion para crear y ver tablero en dos metodos

--- Tablero.java
+++ Tablero.java
@@ -1,8 +1,3 @@
-    /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hundirlaflota;
 
 /**
@@ -13,17 +8,32 @@ public class Tablero {
 
     //--------Declaraciones--------//
     private int [][] medidas = new int [10][10];
-    private int [] barcos;
+    Barco [] nBarcos = new Barco [0];
     
     //--------Metodos--------//
-    void Show(){
+    
+    /*----Crear tabla----*/
+    public void MakeTable(){
         for (int i = 0; i < 10; i++){
             for (int j = 0; j < 10; j++){
+                
                 medidas[i][j]=0;
-
-                System.out.print( (medidas [i] [j]) + " " );
+            }
+        }
+    }
+    
+    public void ShowTable(){
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                System.out.print(medidas[i][j] + " ");
             }
         System.out.println();
         }
     }
+    
+    /*----Generacion de Barcos----*/
+    public void ShipGeneration(){
+        
+    }
+    
 }
