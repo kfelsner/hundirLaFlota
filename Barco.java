ad4a34a2c66f57341f77e31ed5ccc976db0a82f7
kfelsner <kristofersolerfelsner@protonmail.com>
2016-12-02 10:27:01 +0100

    Implementacion de metodos y correccion de errores

--- Barco.java
+++ Barco.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hundirlaflota;
 
 /**
@@ -10,18 +5,66 @@ package hundirlaflota;
  * @author kfelsner
  */
 public class Barco {
-    
+
     //--------Declaraciones--------//
     private int medida;
-    private enum barcos {portaaviones, buque, lancha};
+    enum barcos {portaaviones, buque, lancha};
     private boolean hundido = false;
-    private int [] [] posicionesOcupadas = new int [0][0];
-    private boolean [] [] posicionesHeridas;
-    
+    private int[][] posicionesOcupadas = new int[medida][2];
+    private boolean[] posicionesHeridas = new boolean[medida];
+
     //--------Metodos--------//
-    void MakeShip(Barco.barcos tipo){
-        if (tipo.equals("portaaviones")){
-            
+    /*----Barcos----*/
+    
+    public void PutShip(Barco.barcos tipo, int x, int y, String orientacion) {
+        if (tipo.equals(barcos.portaaviones)) {
+            medida = 5;
+            posicionesOcupadas[x][y] = 1;
+            for (int i = 1; i < medida; i++) {
+                switch (orientacion) {
+                    case "derecha":
+                        posicionesOcupadas[x + i][y] = 1;
+                        break;
+                    case "izquierda":
+                        posicionesOcupadas[x - i][y] = 1;
+                        break;
+                    case "arriba":
+                        posicionesOcupadas[x][y - i] = 1;
+                        break;
+                    case "abajo":
+                        posicionesOcupadas[x][y + i] = 1;
+                        break;
+                }
+            }
+        } else if (tipo.equals(barcos.buque)) {
+            medida = 3;
+            posicionesOcupadas[x][y] = 1;
+            for (int i = 1; i < medida; i++) {
+                switch (orientacion) {
+                    case "derecha":
+                        posicionesOcupadas[x + i][y] = 1;
+                        break;
+                    case "izquierda":
+                        posicionesOcupadas[x - i][y] = 1;
+                        break;
+                    case "arriba":
+                        posicionesOcupadas[x][y - i] = 1;
+                        break;
+                    case "abajo":
+                        posicionesOcupadas[x][y + i] = 1;
+                        break;
+                }
+            }
+        } else if (tipo.equals(barcos.lancha)) {
+            medida = 1;
+            posicionesOcupadas[x][y] = 1;
+        } else {
+            System.out.println("Bad input.");
         }
     }
- }
+
+    /*----Posiciones Heridas----*/
+    public void HurtPosition(int fila, int columna ) {
+        = true;
+    }
+}
