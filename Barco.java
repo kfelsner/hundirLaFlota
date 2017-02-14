071b3e9c9643224dad22897a1773448beaa13b4c
kfelsner <kristofersolerfelsner@protonmail.com>
2017-02-14 08:21:43 +0100

    Creados metodos para colocar barcos, herir y hundir

--- Barco.java
+++ Barco.java
@@ -10,61 +10,105 @@ public class Barco {
     private int medida;
     enum barcos {portaaviones, buque, lancha};
     private boolean hundido = false;
-    private int[][] posicionesOcupadas = new int[medida][2];
-    private boolean[] posicionesHeridas = new boolean[medida];
+    private int[][] posicionesOcupadas;
+    private boolean[][] posicionesHeridas;
 
     //--------Metodos--------//
-    /*----Barcos----*/
     
-    public void PutShip(Barco.barcos tipo, int x, int y, String orientacion) {
-        if (tipo.equals(barcos.portaaviones)) {
+    // ==== WORK IN PROGRESS ==== //
+    
+    //Constructor
+    public Barco (barcos tipo) {
+        if (tipo.equals("portaaviones")) {
             medida = 5;
-            posicionesOcupadas[x][y] = 1;
-            for (int i = 1; i < medida; i++) {
-                switch (orientacion) {
-                    case "derecha":
-                        posicionesOcupadas[x + i][y] = 1;
-                        break;
-                    case "izquierda":
-                        posicionesOcupadas[x - i][y] = 1;
-                        break;
-                    case "arriba":
-                        posicionesOcupadas[x][y - i] = 1;
-                        break;
-                    case "abajo":
-                        posicionesOcupadas[x][y + i] = 1;
-                        break;
-                }
-            }
-        } else if (tipo.equals(barcos.buque)) {
+            posicionesOcupadas = new int[medida][2];
+        }
+        if (tipo.equals("buque")) {
             medida = 3;
-            posicionesOcupadas[x][y] = 1;
-            for (int i = 1; i < medida; i++) {
-                switch (orientacion) {
-                    case "derecha":
-                        posicionesOcupadas[x + i][y] = 1;
-                        break;
-                    case "izquierda":
-                        posicionesOcupadas[x - i][y] = 1;
-                        break;
-                    case "arriba":
-                        posicionesOcupadas[x][y - i] = 1;
-                        break;
-                    case "abajo":
-                        posicionesOcupadas[x][y + i] = 1;
-                        break;
-                }
-            }
-        } else if (tipo.equals(barcos.lancha)) {
-            medida = 1;
-            posicionesOcupadas[x][y] = 1;
-        } else {
-            System.out.println("Bad input.");
+            posicionesOcupadas = new int[medida][2];
+        }
+        if (tipo.equals("lancha")) {
+            medida = 2;
+            posicionesOcupadas = new int[medida][2];
         }
     }
-
-    /*----Posiciones Heridas----*/
-    public void HurtPosition(int fila, int columna ) {
-        = true;
+    
+    //Colocar barco en el tablero.
+    public void colocarBarcos(){
+        
+        int x = (int) (Math.random() * 10);
+        int y = (int) (Math.random() * 10);
+        
+        for (int k = 0; k < 1; k++) {
+            if (!Tablero.tabla[x][y].contenido) {
+                Tablero.tabla[x][y].forma = '*';
+                Tablero.tabla[x][y].contenido = true;
+            }
+        }
     }
+    
+    public void hundido() {
+        hundido = true;
+    }
+    
+    public void herido(int x, int y){
+        posicionesHeridas[x][y] = true;
+    }
+    
 }
+    
+    
+    
+    
+//    public void ponerBarco(Barco.barcos tipo, int x, int y, String orientacion) {
+//        if (tipo.equals(barcos.portaaviones)) {
+//            medida = 5;
+//            posicionesOcupadas[x][y] = 1;
+//            for (int i = 1; i < medida; i++) {
+//                switch (orientacion) {
+//                    case "derecha":
+//                        posicionesOcupadas[x + i][y] = 1;
+//                        break;
+//                    case "izquierda":
+//                        posicionesOcupadas[x - i][y] = 1;
+//                        break;
+//                    case "arriba":
+//                        posicionesOcupadas[x][y - i] = 1;
+//                        break;
+//                    case "abajo":
+//                        posicionesOcupadas[x][y + i] = 1;
+//                        break;
+//                }
+//            }
+//        } else if (tipo.equals(barcos.buque)) {
+//            medida = 3;
+//            posicionesOcupadas[x][y] = 1;
+//            for (int i = 1; i < medida; i++) {
+//                switch (orientacion) {
+//                    case "derecha":
+//                        posicionesOcupadas[x + i][y] = 1;
+//                        break;
+//                    case "izquierda":
+//                        posicionesOcupadas[x - i][y] = 1;
+//                        break;
+//                    case "arriba":
+//                        posicionesOcupadas[x][y - i] = 1;
+//                        break;
+//                    case "abajo":
+//                        posicionesOcupadas[x][y + i] = 1;
+//                        break;
+//                }
+//            }
+//        } else if (tipo.equals(barcos.lancha)) {
+//            medida = 1;
+//            posicionesOcupadas[x][y] = 1;
+//        } else {
+//            System.out.println("Bad input.");
+//        }
+//    }
+//
+//    /*----Posiciones Heridas----*/
+//    public void posicionHerida(int fila, int columna ) {
+//        
+//    }
+//}
