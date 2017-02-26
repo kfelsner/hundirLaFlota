731c47cc82cdf8f51137c7ea05da358fde514902
kfelsner <kristofersolerfelsner@protonmail.com>
2017-02-26 10:57:03 +0100

    Añadidos una serie de metodos.

--- Tablero.java
+++ Tablero.java
@@ -7,11 +7,16 @@ package hundirlaflota;
 public class Tablero {
 
     //--------Declaraciones--------//
-    public static Casillas[][] tabla = new Casillas[10][10];
-    Barco[] nBarcos = new Barco[0];
+    public static int xMax = 10, yMax = 10;
+    public static Casillas[][] tabla = new Casillas[xMax][yMax];
+    public Barco[] nBarcos = new Barco[0];
 
     //--------Metodos--------//
-    // ==== WORK IN PROGRESS ==== //
+    
+
+// ==== WORK IN PROGRESS ==== //
+    
+    
     /*----Crear tabla----*/
     public void crearTablero() {
         for (int x = 0; x < 10; x++) {
@@ -27,6 +32,28 @@ public class Tablero {
                 System.out.print(tabla[x][y].forma + " ");
             }
             System.out.println();
+        }
+    }
+    
+    //Comprueba si las coordenadas estan dentro del tablero.
+    public boolean dentro(int x, int y) {
+        return (!((x<0)||(x>=xMax)||(y<0)||(y>=yMax)));
+    }
+    
+    //Colocar barco en el tablero.
+    public void colocarBarco(int medida) {
+
+        int yIni = (int) (Math.random() * Tablero.yMax); //xIni e yIni es desde donde parte el barco.
+        int xIni = (int) (Math.random() * Tablero.xMax);
+
+        for (int k = 0; k < medida; k++) {
+            if (!Tablero.tabla[yIni][xIni].contenido) {
+                Tablero.tabla[yIni][xIni].forma = '*';
+                Tablero.tabla[yIni][xIni].contenido = true;
+                //posicionesOcupadas = new int[yIni][xIni];
+            } else {
+                k--;
+            }
         }
     }
 }
